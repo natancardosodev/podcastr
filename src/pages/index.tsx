a7f5b3fb3baaaf4a8211f8281fbaa710b596c4da
Natan Cardoso <natancb7@gmail.com>
2021-05-15 17:02:25 -0300

    SPA x SSR x SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-export default function Home() {
+// import { useEffect } from "react";
+
+export default function Home(props) {
+  // SPA
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }, []);
+
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <pre>{JSON.stringify(props)}</pre>
+    </div>
   )
+}
+
+// SSG x SSR
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
